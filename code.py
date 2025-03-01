e07a8126487d46676fba486928bfc38bbcdbe77c
aarxiss <grecanikolay06@gmail.com>
2025-03-01 13:02:31 +0300

    Create code.py

---
+++ code.py
@@ -0,0 +1,10 @@
+*** print("Hello, Python!") ***
+
+*** name = input("Введіть ваше ім'я: ")
+print(f"Привіт, {name}!") ***
+
+*** num1 = float(input("Введіть перше число: "))
+num2 = float(input("Введіть друге число: "))
+sum_result = num1 + num2
+print(f"Сума чисел {num1} та {num2} дорівнює {sum_result}.") ***
+
